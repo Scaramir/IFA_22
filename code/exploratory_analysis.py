1a5fbf2452195b386b6eb1cf6508035ca328abf6
Scaramir <maxotto45@gmail.com>
2022-10-28 18:40:25 +0200

    added todos. colorful plots

--- code/exploratory_analysis.py
+++ code/exploratory_analysis.py
@@ -7,6 +7,8 @@ import random # for reproducibility
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+
 
 # set seeds for reproducibility
 def set_seeds(seed = 1129142087):
@@ -22,8 +24,16 @@ def read_data(filename):
     return df
 
 # replace '?' with None
-def replace_question_mark(df):
+def impute_question_marks(df):
     df.replace('?', None, inplace=True)
+    # convert data type of all columns to float
+    df = df.astype(float)
+    # goal is a categorical variable, so convert it to str for better visualization in pairplot etc.
+    df.goal = df.goal.astype(str)
+    print("Missing values: ", df.isnull().sum())
+    print("Duplicate values: ", df.duplicated().sum())
+    # impute missing values with mean
+    df.fillna(df.mean(), inplace=True)
     return df
 
 # check the data
@@ -31,11 +41,6 @@ def check_data(df):
     print(df.head())
     print(df.info())
     print(df.describe())
-    return
-
-# check the missing values
-def check_missing(df):
-    print("Missing values: ", df.isnull().sum())
     return
 
 # check the correlation
@@ -46,63 +51,69 @@ def check_corr(df):
 
 # check the distribution of the data
 def check_dist(df):
-    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
-    sns.distplot(df['sepal_length'], ax=ax[0, 0])
-    plt.show()
-    return
-
-# check the boxplot
-def check_boxplot(df):
-    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
-    sns.boxplot(df['sepal_length'], ax=ax[0, 0])
+    sns.countplot(x='goal', data=df)
     plt.show()
     return
 
 # check the pairplot
 def check_pairplot(df):
-    #sns.pairplot(df, hue='species')
-    sns.pairplot(df)
+    sns.pairplot(df, hue='goal')
     plt.show()
     return
 
 # check the heatmap
 def check_heatmap(df):
     corr = df.corr()
-    #sns.heatmap(corr, annot=True)
     sns.heatmap(corr)
     plt.show()
     return
 
-# check the scatterplot
-def check_scatterplot(df):
-    #sns.scatterplot(x='sepal_length', y='sepal_width', data=df, hue='species')
-    sns.scatterplot(x='sepal_length', y='sepal_width', data=df)
-    plt.show()
+# split the data into train and test
+# return train and test dataframes
+def split_data(df):
+    return train_test_split(df, test_size=0.2)
+
+
+
+# TODO: 
+# why no PCA?
+# Task 2: Set up some classifiers and evaluate them
+# Evaluation: Accuracy, Sensitivity&Sepcificity (why not Precision&Recall?), F1-Score for unbalanced data
+
+# multiple linear regression 
+def multiple_linear_regression(train, test):
+    return 
+
+# k-NN
+def k_nearest_neighbors(train, test):
     return
 
+# decision tree
+def decision_tree(train, test):
+    return
 
+# TODO: 
+# task 3: same again, but all !=0 goal-values are set to 1. No need for F1-Score anymore.
+# task 4: ROC(&AUC?) curves
+# task 5: Report and presentation 
+
+# ------------------ main ------------------
 # main function
 def main():
     # read the data
     df = read_data('./../data\processedWithHeader.cleveland.data')
-    # replace '?' with None
-    df = replace_question_mark(df)
+    # replace '?' with None, check for missings, and impute missing values with mean
+    df = impute_question_marks(df)
     # check the data
     check_data(df)
-    # check the missing values
-    check_missing(df)
     # check the correlation
     check_corr(df)
-    # check the distribution of the data
-    #check_dist(df)
-    # check the boxplot
-    #check_boxplot(df)
-    # check the pairplot
-    check_pairplot(df)
+    # check the distribution of the data regarding the goal
+    check_dist(df)
     # check the heatmap
     check_heatmap(df)
-    # check the scatterplot
-    #check_scatterplot(df)
+    # check the pairplot
+    check_pairplot(df)
     return df
 
 
