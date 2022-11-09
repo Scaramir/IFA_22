e182f8ce13265b001bc0466defc821772414ef0b
Scaramir <maxotto45@gmail.com>
2022-11-09 12:39:42 +0100

    trying to convert this repo in a package.

--- Project 2/code/mo_nn_helpers.py
+++ Project 2/code/mo_nn_helpers.py
@@ -39,3 +39,132 @@ def get_mean_and_std(data_dir):
     std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
     print("Mean: ", mean, ", Std: ", std)
     return mean, std
+
+
+# Save the whole model
+def save_model(model, dir, model_name):
+    torch.save(model, f"{dir}/{model_name}.pt")
+
+# Load the whole model
+def load_model(model_path, model_name):
+    model = torch.load(f"{model_path}/{model_name}.pt")
+    print(f"Loaded model {model_name}")
+    return model
+
+
+# ----------------------------------Plots-------------------------------
+# These functions will be used in the train_nn function
+def plot_loss(train_losses, test_losses, output_model_name, output_model_path):
+    import matplotlib.pyplot as plt
+    '''Plot the loss per epoch of the training and test set'''
+    plt.clf()
+    plt.plot(train_losses, "-o")
+    plt.plot(test_losses, "-o")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend(["Train", "Test"])
+    plt.title("Train vs. Test Loss")
+    plt.savefig(
+        f"{output_model_path}/train_test_loss_{output_model_name}.png")
+    return
+
+def plot_accuracy(train_accus, test_accus, output_model_name, output_model_path):
+    '''Plot the accuracy per epoch of the training and test set'''
+    import matplotlib.pyplot as plt
+    plt.clf()
+    plt.plot(train_accus, "-o")
+    plt.plot(test_accus, "-o")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    plt.legend(["Train", "Test"])
+    plt.title("Train vs. Test Accuracy")
+    plt.savefig(
+        f"{output_model_path}/train_test_accuracy_{output_model_name}.png")
+    return
+# ---------------------------------------------------------------------
+
+# -------------------------------Training ResNet and ResNeXt-------------------------------
+def train_nn(model, dataloaders, dataset_sizes, criterion, optimizer, scheduler, output_model_path, output_model_name, num_epochs=25):
+    import time
+    import copy
+    import torch
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if device == "cuda":
+        torch.cuda.empty_cache()
+    try_make_dir(output_model_path)
+    since = time.time()
+    # Keep track of accuracy and loss
+    best_model_wts = copy.deepcopy(model.state_dict())
+    train_losses = []
+    train_accus = []
+    test_losses = []
+    test_accus = []
+    best_acc = 0.0
+
+    for epoch in range(1, num_epochs + 1):
+        print("Epoch {}/{}".format(epoch, num_epochs))
+        print('-' * 10)
+        # Each epoch has a training and validation phase
+        for phase in ["train", "test"]:
+            if phase == "train":
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+            running_loss = 0.0
+            running_corrects = 0
+            # Iterate over data.
+            for inputs, labels in tqdm(dataloaders[phase], desc=phase):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                # zero the parameter gradients
+                optimizer.zero_grad(set_to_none=True)
+                # forward
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, pred_labels = torch.max(outputs, 1)        # only checking for correct predictions / label with highest prediction value
+                    loss = criterion(outputs, labels)
+                    # backward + optimize only if in training phase
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+                        scheduler.step()
+                # track stats
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(pred_labels == labels.data)
+
+            # stats for epoch
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = 100. * running_corrects.double() / dataset_sizes[phase]
+            if phase == "train":
+                train_losses.append(epoch_loss)
+                train_accus.append(epoch_acc.item())
+            else:
+                test_losses.append(epoch_loss)
+                test_accus.append(epoch_acc.item())
+            print('{} Loss: {:.3f} Acc: {:.4f}%'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the best model
+            if phase == "test" and epoch_acc >= best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+        print()
+
+    time_elapsed = time.time() - since
+    print("Training complete in {:.0f}m {:.0f}s".format(
+        time_elapsed // 60, time_elapsed % 60))
+    print("Best val Acc: {:4f}".format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    print("----Done----")
+
+    # Plot training curves and save the model
+    plot_loss(train_losses, test_losses, output_model_name, output_model_path)
+    plot_accuracy(train_accus, test_accus, output_model_name, output_model_path)
+    save_model(model, output_model_path, output_model_name)
+    print(">> Model saved as: ", output_model_name)
+    print(">> Model saved in: ", output_model_path)
+
+    return model
+# ----------------------------------------------------------------------
--- Project 2/code/nn_pytorch.py
+++ Project 2/code/nn_pytorch.py
@@ -40,6 +40,7 @@ import time, random
 
 from tqdm import tqdm
 from mo_nn_helpers import get_mean_and_std
+from mo_nn_helpers import *
 #from "./../../Project 1/code/exploratory_analysis_and_classifiers.py" import *
 # TODO: __initi__.py file to reuse code from project 1
 
@@ -104,17 +105,8 @@ num_classes = len(class_names)
 # --------------------------------------------
 
 
-# Save the whole model
-def save_model(model, dir, model_name):
-    torch.save(model, f"{dir}/{model_name}.pt")
-
-# Load the whole model
-def load_model(model_path, model_name):
-    model = torch.load(f"{model_path}/{model_name}.pt")
-    print(f"Loaded model {model_name}")
-    return model
-
 def get_model(model_type, load_trained_model, reset_classifier_with_custom_layers, num_classes=num_classes, pretrained=True, device='cuda', input_model_path=None, input_model_name=None):
+    print('Loading model...')
     if (load_trained_model) & (input_model_path is not None) & (input_model_name is not None):
         model = load_model(input_model_path, input_model_name).to(device)
         print('Loaded model \'{}\'.'.format(input_model_name))
@@ -161,6 +153,7 @@ def get_model(model_type, load_trained_model, reset_classifier_with_custom_layer
                                     nn.ReLU(inplace=True),
                                     nn.Linear(100, num_classes))
     model = model.to(device)
+    print("Done.")
     return model
 model = get_model(model_type='resnet18', load_trained_model=False, reset_classifier_with_custom_layers=True, num_classes=num_classes, pretrained=True, device='cuda', input_model_path=None, input_model_name=None)
 
@@ -168,70 +161,70 @@ criterion = nn.CrossEntropyLoss()
 # SGD optimizer with momentum could lead faster to good results, but Adam is more stable
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+ 
 
-# ----------------------------------Plots-------------------------------
-# These functions will be used in the train_nn function
-def plot_loss(train_losses, test_losses, output_model_name, output_model_path):
-    '''Plot the loss per epoch of the training and test set'''
-    plt.clf()
-    plt.plot(train_losses, "-o")
-    plt.plot(test_losses, "-o")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.legend(["Train", "Test"])
-    plt.title("Train vs. Test Loss")
-    plt.savefig(
-        f"{output_model_path}/train_test_loss_{output_model_name}.png")
-    return
+model = train_nn(model, criterion, optimizer, exp_lr_scheduler, num_epochs=num_epochs, device=device, dataloaders=dataloaders, dataset_sizes=dataset_sizes, class_names=class_names, save_model=True, save_model_path=output_model_path, save_model_name=output_model_name)
 
-def plot_accuracy(train_accus, test_accus, output_model_name, output_model_path):
-    '''Plot the accuracy per epoch of the training and test set'''
-    plt.clf()
-    plt.plot(train_accus, "-o")
-    plt.plot(test_accus, "-o")
-    plt.xlabel("Epoch")
-    plt.ylabel("Accuracy (%)")
-    plt.legend(["Train", "Test"])
-    plt.title("Train vs. Test Accuracy")
-    plt.savefig(
-        f"{output_model_path}/train_test_accuracy_{output_model_name}.png")
-    return
-# ---------------------------------------------------------------------
-
-
-# TODO: Train loop that also saves the best model so far after each epoch via a deepcopy of the model
-def train_nn(model, dataloaders, dataset_sizes, criterion, optimizer, scheduler, output_model_path, output_model_name, num_epochs = 25):
-    # clear the cuda cache
-    # set up empty lists for our accuracy and loss values
-    # for each epoch we will train and test our model, just like the pytorch-tutorial did    
-        # for epoch in range(num_epochs):
-        #    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        #    print('-' * 10)
-        #    # Each epoch has a training and validation phase
-        #    for phase in ['train', 'val']:
-        #        # Iterate over data.
-        #        for inputs, labels in dataloaders[phase]:
-        #            # all to the GPU / to cuda
-        #            # zero the parameter gradients
-        #            # forward pass of each batch of data
-        #            # track history if only in train
-        #            with torch.set_grad_enabled(phase == 'train'):
-        #                # let the model predict the outputs
-        #                # backward (backpropagation) + optimize/lr-scheduler/etc. for train
-        #            # statistics for test
-        #            # saving
-
-    return model 
 
 # TODO: Evaluation of 3 different networks. Use Sigmoid and max to get the probabilities for each of the binary classes.
-def evaluate_model(model, dataloaders, dataset_sizes, criterion, device):
+def evaluate_model(model, dataset_sizes, criterion, class_names, image_datasets, device="cuda", dataset = "test"):
     # for every image of our test set, we will prdict the class and the probability
     # save the probabilities and the classes in a list
     # save the ground truth classes in a list
     # calculate the loss and the accuracy
     # plot the confusion matrix with the older heatmap function from project 1. 
     # plot the ROC curve with the function from project 1
-    return model
+    
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
+                    shuffle=False, num_workers=0)
+                    for x in ["train", "test"]}
+
+    num_samples = 0
+    num_correct = 0
+    true_labels_list = []
+    pred_labels_list = []
+    pred_scores_list = []
+    file_names_list = []
+    model.eval()
+    
+    if device == "cuda":
+        model.cuda.empty_cache()
+    
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(tqdm(dataloaders[dataset]), desc="Evaluating the model..."):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            scores = torch.sigmoid(outputs)
+            scores = torch.max(scores, 1)
+            pred_scores, pred_labels = scores
+
+            num_correct = torch.sum(pred_labels == labels.data)
+            num_samples += pred_labels.size(0)
+            true_labels_list.append(class_names[labels.cpu().detach().numpy()[0]])
+            pred_labels_list.append(class_names[pred_labels.cpu().detach().numpy()[0]])
+            pred_scores_list.append(pred_scores.cpu().detach().numpy()[0])
+            file_names_list.append(image_datasets[dataset].imgs[i][0].split("/")[-1])
+
+    accuracy = num_correct / num_samples
+    loss = criterion(outputs, model)
+    print("Accuracy: {:.2f} %".format(accuracy * 100))
+    print("Loss: {:.2f}".format(loss))
+
+    print(classification_report(true_labels_list, pred_labels_list, target_names=class_names))
+    print(confusion_matrix(true_labels_list, pred_labels_list, labels=class_names))
+    
+
+    df = pd.DataFrame({
+        "file_name": file_names_list,
+        "true_label": true_labels_list,
+        "pred_label": pred_labels_list,
+        "pred_score": pred_scores_list})
+    print("Done.")
+    return df
+
+
+
 
 # TODO: Plot the results. (Also with a confusion matrix as heatmap?)
 
